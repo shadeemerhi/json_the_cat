9c288043df6c052f817e3b6b3a53df88840a4c79
vagrant <shadmerhi@gmail.com>
2020-09-24 07:12:39 +0000

    Able to fetch breed data dynamically with user input

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,23 @@
 const request = require('request');
+
+const breed = process.argv.slice(2).join(' ');
+
+// DYNAMIC API ENDPOINT
+let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+request(url, (error, response, body) => {
+
+  if(error) {
+    console.error(error);
+    process.exit();
+  }
+
+  const data = JSON.parse(body);
+  if(Object.keys(data).length === 0) {
+    console.log('Breed not found. Please try again');
+    process.exit();
+  }
+  
+  const desc = data[0].description;
+  console.log(desc);
+});
